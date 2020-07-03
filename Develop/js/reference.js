196bd33901122344863a32eee6b106fcede3c262
JAVI <ortiz.javier10@gmail.com>
2020-07-03 10:25:29 -0400

    first commit

---
+++ Develop/js/reference.js
@@ -0,0 +1,7 @@
+// var currentDate = moment();
+// //     console.log(currentDate);
+
+
+var addDate = document.getElementById('currentDay');
+    addDate.innerHTML = moment().format("dddd, MMMM Do");
+//test
